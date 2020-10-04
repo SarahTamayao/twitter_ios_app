f70abe6b5e521b38de40812ffeba2be88a4533de
jomeke <10506008+curiousjazz77@users.noreply.github.com>
2020-10-04 12:59:26 -0500

    Allow user to stay logged in

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,10 +16,19 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        // when the pane shows up
+        // we want to check if the user default is set for the UserLoggedIn key
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
         //print("hello world") //anytime someone clicks on button, it'll print to console
         let myUrl = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: myUrl, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Could not log in!")
